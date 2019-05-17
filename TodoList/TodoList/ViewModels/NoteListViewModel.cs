8a136fea59942aba472ca597627159661544d2cb
bulirm <martin.bulir@pslib.cz>
2019-05-17 21:30:55 +0200

    add basic pages and viewmodels

---
+++ TodoList/TodoList/ViewModels/NoteListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TodoList.ViewModels.Abstract;
+
+namespace TodoList.ViewModels
+{
+    class NoteListViewModel: ViewModel
+    {
+        #region properties
+
+        private List<NoteViewModel> noteViewModels;
+
+        public List<NoteViewModel> NoteViewModels
+        {
+            get { return noteViewModels; }
+            set { noteViewModels = value; OnPropertyChanged(); }
+        }
+
+        #endregion properties
+
+        public NoteListViewModel()
+        {
+
+        }
+    }
+}
---
+++ TodoList/TodoList/ViewModels/NoteViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TodoList.ViewModels.Abstract;
+
+namespace TodoList.ViewModels
+{
+    class NoteViewModel: ViewModel
+    {
+        #region properties
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChanged(); }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; OnPropertyChanged(); }
+        }
+
+        #endregion properties
+
+        public NoteViewModel()
+        {
+
+        }
+    }
+}
